b1aa73fb98f32a5418a8a88684ec97f0057682d8
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-02-08 18:45:04 -0800

    updated username to be limited to 20 characters;

--- feedback.sql
+++ feedback.sql
@@ -7,16 +7,16 @@ CREATE DATABASE feedback;
 
 \c feedback
 
-CREATE TABLE user
+CREATE TABLE users
 (
-    username PRIMARY KEY UNIQUE,
+    username VARCHAR(20) PRIMARY KEY UNIQUE,
     password TEXT NOT NULL,
     email VARCHAR(50) NOT NULL UNIQUE,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL
 );
 
-INSERT INTO user ( username, password, email, first_name, last_name )
+INSERT INTO users ( username, password, email, first_name, last_name )
 VALUES
 ('eddie123', 'eddieaviles123', 'eddie@yahoo.com', 'Eduardo', 'Aviles'),
 ('nancy123', 'nancyaviles123', 'nancy@yahoo.com', 'Nancy', 'Aviles'),
