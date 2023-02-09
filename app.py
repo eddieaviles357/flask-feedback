c53baabb9f9a283570ab76ad303c88f14907cfa7
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-02-08 20:32:36 -0800

    initial imports added and single route that redirects to /register

--- app.py
+++ app.py
@@ -1,14 +1,43 @@
 
-from flask import Flask, render_template, request, flash
-
+from flask import Flask, render_template, request, flash, redirect
+from flask_debugtoolbar import DebugToolbarExtension
+from models import connect_db, User, db
 app = Flask(__name__)
 
 app.config.update(
-
+    SQLALCHEMY_DATABASE_URI="postgresql:///feedback.sql",
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    # SQLALCHEMY_ECHO=True,
+    SECRET_KEY="xxsecretxxkeyxxyesxx",
+    DEBUG_TB_INTERCEPT_REDIRECTS=False
 )
 
+# connect db
+connect_db(app)
+
+debug = DebugToolbarExtension(app)
+# GET / 
+# Redirect to /register.
 
 @app.route('/')
 def home():
     """ Home route """
-    return render_template("index.html")
+    raise
+    return redirect('/')
+
+# GET /register
+# Show a form that when submitted will register/create a user. This form should accept a username, password, email, first_name, and last_name.
+
+# Make sure you are using WTForms and that your password input hides the characters that the user is typing!
+
+# POST /register
+# Process the registration form by adding a new user. Then redirect to /secret
+# GET /login
+# Show a form that when submitted will login a user. This form should accept a username and a password.
+
+# Make sure you are using WTForms and that your password input hides the characters that the user is typing!
+
+# POST /login
+# Process the login form, ensuring the user is authenticated and going to /secret if so.
+# GET /secret
+# Return the text “You made it!” (don’t worry, we’ll get rid of this soon)
