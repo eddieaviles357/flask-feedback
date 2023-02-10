1581013126f59fc459e9f4f46d4c79881257706b
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-02-09 20:48:33 -0800

    added loginform

--- registerform.py
+++ registerform.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SelectField, BooleanField, PasswordField
-from wtforms.validators import InputRequired, Optional, NumberRange, URL, Length
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Length
 
 class RegisterForm(FlaskForm):
     """ Register For User """
@@ -15,3 +15,11 @@ class RegisterForm(FlaskForm):
     first_name = StringField("First Name", validators=[ InputRequired(message="Please Enter a First Name"), Length(max=30)])
 
     last_name = StringField("Last Name", validators=[ InputRequired(message="Please Enter a First Name"), Length(max=30)])
+
+class LoginForm(FlaskForm):
+    """ Loging form """
+    username = StringField("Username", validators=[
+                       InputRequired(message="Please Enter a Username"), Length(max=20)])
+
+    password = PasswordField("Password", validators=[
+                          InputRequired(message="Please Enter a Password")])
