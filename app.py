50bb79cc94856b306567ac80dcb7b337ab604fd3
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-02-09 19:14:08 -0800

    register route added and secret route added(will be changed)

--- app.py
+++ app.py
@@ -24,21 +24,27 @@ debug = DebugToolbarExtension(app)
 @app.route('/')
 def home():
     """ Home route """
-    return redirect('/')
+    return redirect('/register')
 
 # GET /register
-# Show a form that when submitted will register/create a user. This form should accept a username, password, email, first_name, and last_name.
+# Show a form that when submitted will register/create a user. 
+# This form should accept a username, password, email, first_name, and last_name.
 # Make sure you are using WTForms and that your password input hides the characters that the user is typing!
     
-@app.route('/register')
-def register_user():
-    """ Register route ( register a new user )"""
-    form = RegisterForm()
-
-    return render_template('register.html', form=form)
-
 # POST /register
 # Process the registration form by adding a new user. Then redirect to /secret
+@app.route('/register', methods=["GET", "POST"])
+def register_user():
+    """ Register route ( register a new user ) """
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = {k: form.data[k] for k in form.data if k != 'csrf_token'}
+
+        for k in form.data: print(k,form.data[k])
+        flash("Successfully Register", "success")
+        redirect('/secret')
+    return render_template('register.html', form=form)
+
 # GET /login
 # Show a form that when submitted will login a user. This form should accept a username and a password.
 
@@ -46,5 +52,10 @@ def register_user():
 
 # POST /login
 # Process the login form, ensuring the user is authenticated and going to /secret if so.
+
 # GET /secret
 # Return the text “You made it!” (don’t worry, we’ll get rid of this soon)
+@app.route('/secret')
+def secret_route():
+    """ Secret route """
+    return "<h1>You made it!!</h1>"
