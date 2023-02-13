42b22fd1562a38a3dfc33cb9070e0f7b9b42786e
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-02-12 18:40:51 -0800

    updated database with values

--- feedback.sql
+++ feedback.sql
@@ -29,3 +29,11 @@ VALUES
 ('eddie123','$2b$12$DxGUSeUO8T71X4waAGJvRODEXhqmXQmfk2wYAAdJeaSqTMZoizbYO', 'eddie1234@yahoo.com', 'Edward', 'Avile'),
 ('nancy123','$2b$12$A89.yxoTFm/.iA3P9lJB1.wT1ejtzfihguyphNqIdSzpBzURq.z8O', 'nancy1234@yahoo.com', 'Nanc', 'Avile'),
 ('eric123','$2b$12$HenQaqTWBtrO0bI7mffCU.ZyIrjVtBc8hY8nr5.mfVen80FtqzGK.', 'eric1234@yahoo.com', 'Eric', 'Avile');
+
+INSERT INTO feedbacks (title, content, username_key)
+VALUES
+('Testing Feedback1', 'testing content1', 'eddie123'),
+('Feedback2 Test', 'Content2', 'eddie123'),
+('Nancys Feedback1', 'Nancys content1', 'nancy123'),
+('Erics Feedback1', 'Erics content1', 'eric123'),
+('Erics second feedback', 'Testing more with Erirc', 'eric123');
