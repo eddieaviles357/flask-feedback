dd936e4c3805d431196ea773ae1b902b64925e2a
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-02-14 10:47:58 -0800

    redirect_to_login_with_flash_message util function added

--- utils.py
+++ utils.py
@@ -1,4 +1,11 @@
-from flask import session
+from flask import session, flash, redirect
+from form import LoginForm
+
 def is_user_in_session(username):
     """ Checks if user is in session """
     return True if session['username'] == username else False
+
+def redirect_to_login_with_flash_message(msg, category):
+    """ Redirect to Login form route w/ flash messages and category """
+    flash(msg, category)
+    return redirect('/login')
